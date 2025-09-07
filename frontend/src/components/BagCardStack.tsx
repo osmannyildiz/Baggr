bf4c7c15da616c4aa47417d471cc8bb2e4d064f8
osmannyildiz <iamosmannyildiz@gmail.com>
2025-09-07 03:19:20 +0300

    frontend: add swiping

--- frontend/src/components/BagCardStack.tsx
+++ frontend/src/components/BagCardStack.tsx
@@ -1,15 +1,27 @@
+import { TbDeviceUnknown } from "react-icons/tb";
 import type { Bag } from "../types";
-import { BagCard } from "./BagCard";
+import { BagCard, type SwipeDirection } from "./BagCard";
 
 interface Props {
   bags: Bag[];
+  onSwipe: (bagId: string, direction: SwipeDirection) => void;
+  onCardLeftScreen: (bagId: string, direction: SwipeDirection) => void;
 }
 
-export function BagCardStack({ bags }: Props) {
+export function BagCardStack({ bags, onSwipe, onCardLeftScreen }: Props) {
   return (
-    <div className="h-full px-4 pb-2">
+    <div className="relative h-full px-4 pb-2">
+      <div className="absolute inset-0 flex items-center justify-center">
+        <TbDeviceUnknown size={128} />
+      </div>
+
       {bags.map((bag) => (
-        <BagCard key={bag.id} bag={bag} />
+        <BagCard
+          key={bag.id}
+          bag={bag}
+          onSwipe={(direction) => onSwipe(bag.id, direction)}
+          onCardLeftScreen={(direction) => onCardLeftScreen(bag.id, direction)}
+        />
       ))}
     </div>
   );
--- frontend/src/routes/_app/discover.tsx
+++ frontend/src/routes/_app/discover.tsx
@@ -1,4 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { useState } from "react";
+import { toast } from "sonner";
 import { BagCardStack } from "../../components/BagCardStack";
 import { BAGS } from "../../mockData";
 
@@ -7,5 +9,19 @@ export const Route = createFileRoute("/_app/discover")({
 });
 
 function RouteComponent() {
-  return <BagCardStack bags={BAGS} />;
+  const [bags, setBags] = useState(BAGS);
+
+  return (
+    <BagCardStack
+      bags={bags}
+      onSwipe={(bagId, direction) =>
+        direction === "right"
+          ? toast(`Liked bag ${bagId}`)
+          : toast(`Disliked bag ${bagId}`)
+      }
+      onCardLeftScreen={(bagId) => {
+        setBags(bags.filter((bag) => bag.id !== bagId));
+      }}
+    />
+  );
 }
