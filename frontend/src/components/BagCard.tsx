7e7eb0fcf340ff1959da0666cf64a0b4de8f6426
osmannyildiz <iamosmannyildiz@gmail.com>
2025-09-07 03:27:16 +0300

    frontend: fix press

--- frontend/src/components/BagCard.tsx
+++ frontend/src/components/BagCard.tsx
@@ -23,40 +23,40 @@ export function BagCard({ bag, onSwipe, onCardLeftScreen }: Props) {
         onCardLeftScreen(direction as SwipeDirection)
       }
       preventSwipe={["up", "down"]}
-      className="h-full"
+      className="relative z-10 h-full overflow-hidden rounded-xl shadow-xl"
     >
-      <div className="relative h-full overflow-hidden rounded-xl shadow-xl">
+      <div
+        className="absolute h-full w-full"
+        style={{ background: `url('${bag.imageUrl}') #ddd` }}
+      ></div>
+
+      <div className="absolute bottom-0 z-20 flex w-full flex-col bg-linear-to-b from-transparent to-black/80 p-4 pt-12 text-white">
+        {pageIndex === 0 && <BagCardSummaryPage bag={bag} />}
+        {pageIndex === 1 && <BagCardTokensPage />}
+        {pageIndex === 2 && <BagCardAnalysisPage />}
+
+        <div className="mt-4 flex justify-center gap-2">
+          {Array.from({ length: MAX_PAGE_INDEX + 1 }).map((_, index) => (
+            <div
+              key={index}
+              className={cn(
+                "h-2 w-2 rounded-full border-2",
+                pageIndex === index && "bg-white",
+              )}
+            ></div>
+          ))}
+        </div>
+      </div>
+
+      <div className="absolute grid h-full w-full grid-cols-2">
         <div
-          className="absolute h-full w-full"
-          style={{ background: `url('${bag.imageUrl}') #ddd` }}
+          className="pressable"
+          onClick={() => setPageIndex(Math.max(pageIndex - 1, 0))}
         ></div>
-
-        <div className="absolute bottom-0 z-10 flex w-full flex-col bg-linear-to-b from-transparent to-black/80 p-4 pt-12 text-white">
-          {pageIndex === 0 && <BagCardSummaryPage bag={bag} />}
-          {pageIndex === 1 && <BagCardTokensPage />}
-          {pageIndex === 2 && <BagCardAnalysisPage />}
-
-          <div className="mt-4 flex justify-center gap-2">
-            {Array.from({ length: MAX_PAGE_INDEX + 1 }).map((_, index) => (
-              <div
-                key={index}
-                className={cn(
-                  "h-2 w-2 rounded-full border-2",
-                  pageIndex === index && "bg-white",
-                )}
-              ></div>
-            ))}
-          </div>
-        </div>
-
-        <div className="absolute grid h-full w-full grid-cols-2">
-          <div onClick={() => setPageIndex(Math.max(pageIndex - 1, 0))}></div>
-          <div
-            onClick={() =>
-              setPageIndex(Math.min(pageIndex + 1, MAX_PAGE_INDEX))
-            }
-          ></div>
-        </div>
+        <div
+          className="pressable"
+          onClick={() => setPageIndex(Math.min(pageIndex + 1, MAX_PAGE_INDEX))}
+        ></div>
       </div>
     </TinderCard>
   );
